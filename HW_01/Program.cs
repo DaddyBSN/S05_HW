4781b0cc9f5efeecbcc21e24bdf11b5a8803b99b
Daddy <sergoffan@mail.ru>
2022-10-22 16:10:45 +0300

    add HW_01

--- HW_01/Program.cs
+++ HW_01/Program.cs
@@ -1,10 +1,23 @@
 ﻿// Найти точку пересечения двух прямых заданных уравнением y = k1 * x + b1, y = k2 * x + b2, b1 k1 и b2 и k2 заданы
 
-Console.WriteLine("Введи b1");
-double b1 = double.Parse(Console.ReadLine()!);
-Console.WriteLine("Введи k1");
-double k1 = double.Parse(Console.ReadLine()!);
-Console.WriteLine("Введи b2");
-double b2 = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Введи k2");
-double k2 = Convert.ToDouble(Console.ReadLine());
+double[] arrayK = new double[2];
+double[] arrayB = new double[2];
+
+for(int i = 0; i < arrayK.Length; i++)
+{
+   arrayK[i] = new Random().Next(-5, 5);
+   arrayB[i] = new Random().Next(-5, 5);
+   Console.WriteLine($"K{i+1} = {arrayK[i]}, ");
+   Console.WriteLine($"B{i+1} = {arrayB[i]}, ");
+}   
+Console.WriteLine();
+
+if(arrayK[0] == arrayK[1]) 
+Console.WriteLine("Прямые не имеют пересечений");
+
+else
+{
+    double x = (arrayB[1] - arrayB[0]) / (arrayK[0] - arrayK[1]);
+    double y = x * arrayK[0] + arrayB[0];
+    Console.WriteLine($"Координаты пересечения: ({x}, {y})");
+} 
