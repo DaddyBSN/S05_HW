639c47820e8b6fb5332f1e3ea7062cbe4c7f18e1
Daddy <sergoffan@mail.ru>
2022-10-22 13:35:31 +0300

    add HW

---
+++ HW_01/Program.cs
@@ -0,0 +1,10 @@
+﻿// Найти точку пересечения двух прямых заданных уравнением y = k1 * x + b1, y = k2 * x + b2, b1 k1 и b2 и k2 заданы
+
+Console.WriteLine("Введи b1");
+double b1 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введи k1");
+double k1 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введи b2");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введи k2");
+double k2 = Convert.ToDouble(Console.ReadLine());
