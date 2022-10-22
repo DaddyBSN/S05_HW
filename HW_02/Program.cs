d17e407d734c03ad0f13a90138e6ad531fd8c223
Daddy <sergoffan@mail.ru>
2022-10-22 16:21:50 +0300

    add HW_2

--- HW_02/Program.cs
+++ HW_02/Program.cs
@@ -1,2 +1,24 @@
-﻿// Написать программу копирования массива
+﻿// Написать программу масштабирования фигуры
 
+Console.Write("Координаты вершин фигуры: ");
+int[] arrayAxisX = new int[4];
+int[] arrayAxisY = new int[4];
+
+for (int i = 0; i < arrayAxisX.Length; i++)
+{
+    arrayAxisX[i] = new Random().Next(-10, 10);
+    arrayAxisY[i] = new Random().Next(-10, 10);
+    Console.Write($"{(arrayAxisX[i], arrayAxisY[i])} ");
+}
+Console.WriteLine();
+
+Console.Write("Введи коофициент: ");
+int ratio = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine();
+
+for (int i = 0; i < arrayAxisX.Length; i++)
+{
+    arrayAxisX[i] = ratio * arrayAxisX[i];
+    arrayAxisY[i] = ratio * arrayAxisY[i];
+    Console.Write($"{(arrayAxisX[i], arrayAxisY[i])} ");
+}
