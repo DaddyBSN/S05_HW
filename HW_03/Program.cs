95de47109a75f7beb8bed503e7edd0fe43869edf
Daddy <sergoffan@mail.ru>
2022-10-22 16:31:26 +0300

    add HW_03

--- HW_03/Program.cs
+++ HW_03/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу копирования массива
+
+int[] array = new int[5];
+int count = 1;
+
+void FillArray(int[] array)
+{
+    Console.Write($"Копия № {count}: ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = array[i];
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-10, 10);
+    Console.Write($"{array[i]} ");
+}
+Console.WriteLine();
+
+Console.Write("Введи количество копий массива: ");
+int quantity = int.Parse(Console.ReadLine() ?? "0");
+
+while (count <= quantity)
+{
+    FillArray(array);
+    count++;
+}
